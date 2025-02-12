da779a728417bbdabfa76e5dcd9bcd66de8f0a49
lexvdvliet <lvandervliet@clearviewdata.nl>
2025-02-12 09:38:10 +0100

    comments and logging removed

--- accent_color.js
+++ accent_color.js
@@ -1,7 +1,6 @@
 const savedAccentColor = localStorage.getItem('accentColor');
 
 if (savedAccentColor) {
-  // Inject into a style tag early in the head
   const style = document.createElement('style');
   style.textContent = `:root { --accentcolor: ${savedAccentColor}; }`;
   document.head.appendChild(style);
--- accent_color_picker.js
+++ accent_color_picker.js
@@ -2,9 +2,7 @@ const accentColorPicker = document.getElementById('accent-color-picker');
   
 if (accentColorPicker) {
   const accentColorStyle = window.getComputedStyle(accentColorPicker);
-  const accentColorD2S = accentColorStyle.color; // This should return something like 'rgb(52, 152, 219)'
-
-  console.log('Computed color:', accentColorD2S); // Check if it's a valid color value
+  const accentColorD2S = accentColorStyle.color;
 
   // Save the color if it's valid
   if (accentColorD2S !== 'rgba(0, 0, 0, 0)') {
