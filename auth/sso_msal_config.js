51f9d114c1087bd2571812cc627903ea9b191935
lexvdvliet <lvandervliet@clearviewdata.nl>
2025-02-26 10:18:00 +0100

    msal config file

--- auth/sso_msal_config.js
+++ auth/sso_msal_config.js
@@ -1,8 +1,7 @@
-<script> 
 const msalConfig = {
   auth: {
-    clientId: "46b86472-683d-4a28-a6d0-2c69d3e1ce41",
-    authority: "https://login.microsoftonline.com/3790009a-a120-44ed-a6ba-1fdf516e3e43",
+    clientId: "your_client_id",
+    authority: "https://login.microsoftonline.com/your_tenant_id",
     redirectUri: window.location.origin + "/datatoshare-home"
   },
   cache: {
@@ -10,4 +9,3 @@ const msalConfig = {
     storeAuthStateInCookie: false
   }
 };
-</script>
