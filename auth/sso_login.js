473085fd34d7bbc858cbc174821baf4288efea94
lexvdvliet <lvandervliet@clearviewdata.nl>
2025-02-26 09:50:39 +0100

    client id en tenant id dynamisch

--- auth/sso_login.js
+++ auth/sso_login.js
@@ -1,7 +1,7 @@
 const msalConfig = {
   auth: {
-    clientId: "46b86472-683d-4a28-a6d0-2c69d3e1ce41",
-    authority: "https://login.microsoftonline.com/3790009a-a120-44ed-a6ba-1fdf516e3e43",
+    clientId: msal-client-id,
+    authority: `https://login.microsoftonline.com/${msal-tenant-id}`,
     redirectUri: window.location.origin + "/datatoshare-home"
   },
   cache: {
